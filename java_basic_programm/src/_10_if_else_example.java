2cabcc87ce0fc538a6d7d0c45b51b7847bf3a563
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-05-17 16:22:54 +0530

    if else is completed

---
+++ java_basic_programm/src/_10_if_else_example.java
@@ -0,0 +1,12 @@
+public class _10_if_else_example {
+    public static void main(String[] args) {
+        // here we see the  working of if else
+
+        boolean  data = true;
+        if (data){
+            System.out.println("if condition is true so this is execute ");
+        }else{
+            System.out.println("if condition is false then else condition is execute");
+        }
+    }
+}
