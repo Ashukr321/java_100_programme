ca78f04598e24c37414317a72522cdb4ce9dd24d
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-05-17 17:19:36 +0530

    _14_factorial_of_number

---
+++ java_basic_programm/src/_14_factorial_of_number.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class c {
+    public static void main(String[] args) {
+        // find the factorial of the number
+        //create the object of the class
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter your number");
+        int n = input.nextInt();
+        int fact = 1;
+        for (int c = 1; c<=n;c++){
+                fact =+ fact*c;
+
+        }
+        System.out.println("the factorial of the your number is : " + fact);
+
+    }
+}
