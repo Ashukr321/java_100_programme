842cc53e872bb48c77b836df4450231f8de78681
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-05-17 10:51:13 +0530

    command line argument is completed

---
+++ java_basic_programm/src/_3_command_line_arguments.java
@@ -0,0 +1,8 @@
+public class _3_command_line_arguments {
+    public static void main(String[] args) {
+        // here we see about the command line argument
+        for (String t:args){
+            System.out.println(t);
+        }
+    }
+}
