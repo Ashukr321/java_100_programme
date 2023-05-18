18a7479e8128bc4200cdd7f1ac58fdba51a995ba
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-05-18 16:24:51 +0530

    _23_print_all_alphabets_a_to_z  is completed

---
+++ java_basic_programm/src/_23_print_all_alphabets_a_to_z.java
@@ -0,0 +1,9 @@
+public class _23_print_all_alphabets_a_to_z {
+    public static void main(String[] args) {
+        // printing the all the alphabets from a to z
+        char ch ; // char take 2 byte -> 16 bits in the memory
+        for (ch= 'a'; ch<='z'; ch++){
+            System.out.println(ch);
+        }
+    }
+}
