c701fc473c586278a5d66351591de7033ee53932
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-05-18 16:34:14 +0530

    enhacne for loop is completed

---
+++ java_basic_programm/src/_24_enhance_loop.java
@@ -0,0 +1,9 @@
+public class _24_enhance_loop {
+    public static void main(String[] args) {
+        // here we see  enhance loop
+         int num [] = {2,33,5,35,35,3,3,5,32 ,6,6,6,54,5};
+         for (int x :num){
+             System.out.print(x +" , ");
+         }
+    }
+}
